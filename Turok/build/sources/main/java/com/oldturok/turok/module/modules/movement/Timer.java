02c88133a9b28ab39cf21536c4115fde64e7f47b
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2020-03-01 13:50:05 -0300

    Timer coded.

--- Turok/build/sources/main/java/com/oldturok/turok/module/modules/movement/Timer.java
+++ Turok/build/sources/main/java/com/oldturok/turok/module/modules/movement/Timer.java
@@ -6,7 +6,7 @@ import com.oldturok.turok.setting.Settings;
 
 @Module.Info(name = "Timer", category = Module.Category.TUROK_MOVEMENT)
 public class Timer extends Module {
-	private Setting<Integer> ticks_speed_value = register(Settings.integerBuild("Time Value".withMinimum(0).withValue(4).withMaximum(20)));
+	private Setting<Integer> ticks_speed_value = register(Settings.integerBuilder("Time Value").withMinimum(0).withValue(4).withMaximum(20));
 
 	@Override
 	public void onDisable() {
--- Turok/src/main/java/com/oldturok/turok/module/modules/movement/Timer.java
+++ Turok/src/main/java/com/oldturok/turok/module/modules/movement/Timer.java
@@ -4,17 +4,18 @@ import com.oldturok.turok.module.Module;
 import com.oldturok.turok.setting.Setting;
 import com.oldturok.turok.setting.Settings;
 
+// Rina.
 @Module.Info(name = "Timer", category = Module.Category.TUROK_MOVEMENT)
 public class Timer extends Module {
-	private Setting<Integer> ticks_speed_value = register(Settings.integerBuild("Time Value".withMinimum(0).withValue(4).withMaximum(20)));
+	private Setting<Integer> ticks_speed_value = register(Settings.integerBuilder("Time Value").withMinimum(0).withValue(4).withMaximum(20));
 
 	@Override
 	public void onDisable() {
-		// mc.timer.tickLength = 50;
+		mc.timer.tickLength = 50;
 	}
 
 	@Override
 	public void onUpdate() {
-		// mc.timer.tickLength = (50.0f / ticks_speed_value.getValue());
+		mc.timer.tickLength = (50.0f / ticks_speed_value.getValue());
 	}
 }
