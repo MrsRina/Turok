a419e51e2f7407b08f59eed7d752413e18a8cbd8
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2020-03-18 00:06:01 -0300

    Annouc : )

--- Turok/build/sources/main/java/com/oldturok/turok/modules/modules/chat/AnnoucSpam.java
+++ Turok/build/sources/main/java/com/oldturok/turok/modules/modules/chat/AnnoucSpam.java
@@ -30,49 +30,53 @@ import net.minecraft.item.ItemFood;
 import net.minecraft.block.Block;
 import net.minecraft.init.Items;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.text.DecimalFormat;
-import java.math.RoundingMode;
-import java.util.TimerTask;
-import java.util.Timer;
-import java.util.Queue;
-import java.util.Map;
+import java.util.Random;
 
 @Module.Info(name = "AnnoucSpam", category = Module.Category.TUROK_CHAT)
 public class AnnoucSpam extends Module {
-	private Setting<Integer> tick_ = register(Settings.integerBuilder("Tick").withRange(1, 10).withValue(10).build());
-	private Setting<Integer> delay = register(Settings.integerBuilder("Delay").withRange(500, 5000).withValue(1).build());
+	private Setting<Integer> tick_ = register(Settings.integerBuilder("Tick").withRange(1, 50).withValue(10).build());
+	private Setting<Integer> delay = register(Settings.integerBuilder("Delay").withRange(2000, 10000).withValue(2000).build());
 
 	public int x;
 	public int z;
 
-	public int old_x;
-	public int old_z;
-
-	public int moved;
+	public float moved;
 
 	public int tick;
+
+	public int no_spam;
+
+	public boolean send_m = false;
+	public boolean moving = true;
 
 	public void update_tick() {
 		tick += tick_.getValue();
 
-		x = (int) mc.player.posX;
-		z = (int) mc.player.posZ;
+		Random gerador = new Random(19700621);
 
-		old_x = x;
-		old_z = z;
-
-		if (old_x != mc.player.posX || old_z != mc.player.posZ) {
-			moved++;
+		if (mc.player.movementInput.moveForward == 0.0f && mc.player.movementInput.moveStrafe == 0.0f) {
+			moved  = 0.1f;
+			send_m = false;
+		} else {
+			moved  = 0.0f;
+			send_m = true;
 		}
 
 		if (tick >= delay.getValue()) {
-			send("I walked " + Integer.toString(moved) + ", thanks Turok.");
+			if (send_m) {
+				send("I walked " + Float.toString(moved) + ", thanks Turok.");
+
+			} else {
+				send("Im just stoped, thanks Turok. My cache money: " + Integer.toString(gerador.nextInt(5000)));
+
+
+			}
 
 			tick = 0;
 
-			update_tick();
+			if (moving) {
+				update_tick();
+			}
 		}
 	}
 
